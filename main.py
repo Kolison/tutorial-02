be086e376fa9fd7413e9b6f5fa6650597f1a4922
0Sordex0 <stasglogal6@gmail.com>
2023-06-06 13:13:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 is_active = input("Is the user active? ")
 is_admin = input("Is the user administrator? ")
 is_permission = input("Does the user have access? ")
+
 if is_admin or is_permission == True:
     result = is_active == True
     result = is_permission == True
-    print: (123)
+    print: ("Welcome")
 else: 
-    print (321)
+    print (0)
 access = True
