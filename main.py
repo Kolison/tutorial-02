e7ab5353f854f9069af3d374b3defaea058b22d4
0Sordex0 <stasglogal6@gmail.com>
2023-06-06 13:12:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-for x in range (1, 6):
-    print(x)
+is_active = input("Is the user active? ")
+is_admin = input("Is the user administrator? ")
+is_permission = input("Does the user have access? ")
+if is_admin or is_permission == True:
+    result = is_active == True
+    result = is_permission == True
+    print: (123)
+else: 
+    print (321)
+access = True
